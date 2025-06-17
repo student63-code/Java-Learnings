f329868304a5bea4bcce6b9423025820df63153c
student63-code <studentqwicklabs@gmail.com>
2025-06-17 23:04:39 +0530

    Add files via upload

---
+++ FirstClassMain.java
@@ -0,0 +1,11 @@
+// output println 
+    public class FirstClassMain {
+    public static void main(String[] args) {
+        System.out.println("My World");
+        System.out.println("Sakshi Joshi");
+        System.out.println( "Discover New Ideas");
+        System.out.println("I am learning Java.");
+    System.out.println("It is awesome!");
+    }
+}
+
---
+++ Readme.md
@@ -0,0 +1,5 @@
+Java Concepts
+
+Learnt About Output 
+
+Println features 
